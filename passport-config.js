58cd9cb5b3cb0913877c8647344fc532726673b5
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2020-09-23 17:14:45 +0200

    Switched to bcryptjs for VPS errors reason

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 function initialize(passport, getUserByEmail, getUserById) {
     passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
